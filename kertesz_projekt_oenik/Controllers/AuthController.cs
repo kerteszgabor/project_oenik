f3001009534896b809886caf4875b39db6ad5be1
Kertész Gábor <masodemailcim@gmail.com>
2020-11-17 20:17:22 +0100

    Add full CRUD methods to user handler controller

--- kertesz_projekt_oenik/Controllers/AuthController.cs
+++ kertesz_projekt_oenik/Controllers/AuthController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using kertesz_projekt_oenik.Data;
 using kertesz_projekt_oenik.Models;
 using kertesz_projekt_oenik.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -17,52 +19,43 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace kertesz_projekt_oenik.Controllers
 {
+    [Route("users")]
     public class AuthController : Controller
     {
         private readonly UserManager<User> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext applicationDbContext;
 
-        public AuthController(UserManager<User> userManager, IConfiguration configuration, RoleManager<IdentityRole> roleManager)
+        public AuthController(UserManager<User> userManager, IConfiguration configuration, ApplicationDbContext context)
         {
             _userManager = userManager;
-            _roleManager = roleManager;
             _configuration = configuration;
+            applicationDbContext = context;
         }
 
         [Route("register")]
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<ActionResult> InsertUser([FromBody] RegisterViewModel model)
         {
             var currentUserName = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
-            if (currentUserName == "admin")
-            {
-                User currentUser = _userManager.FindByNameAsync(currentUserName).Result;
-                var user = new User
-                {
-                    Email = model.Email,
-                    UserName = model.Username,
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    NormalizedUserName = model.Username.ToUpper(),
-                    NormalizedEmail = model.Email.ToUpper(),
-                    CreatedOn = DateTime.Now,
-                    CreatedBy = currentUser,
-                    EmailConfirmed = true
-                };
-                var result = await _userManager.CreateAsync(user, model.Password);
+            User currentUser = _userManager.FindByNameAsync(currentUserName).Result;
 
-                if (result.Succeeded)
-                {
-                    await _userManager.AddToRoleAsync(user, model.Role);
-                }
+            var user = CreateUserFromViewModel(model);
+            user.CreatedOn = DateTime.Now;
+            user.ModifiedOn = DateTime.Now;
+            user.CreatedBy = currentUser;
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, model.Role);
                 return Ok(new { Username = user.UserName });
             }
             else
             {
-                return Forbid();
+                return BadRequest(result.Errors.ToList());
             }
         }
 
@@ -104,6 +97,78 @@ namespace kertesz_projekt_oenik.Controllers
                   });
             }
             return Unauthorized();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IEnumerable<User>> List()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+
+        [HttpGet("{username}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<User> GetUserByUsername(string username)
+        {
+            return await _userManager.FindByNameAsync(username);
+        }
+
+        [HttpDelete("{uid}")]
+        [Route("delete")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(string uid)
+        {
+            var user = await _userManager.FindByIdAsync(uid);
+            if (user != null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest(result.Errors.ToList());
+                }
+            }
+            else
+            {
+                return NotFound("User not found");
+            }
+        }
+
+        [HttpPatch("{uid}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Update(string uid, [FromBody] JsonPatchDocument<User> patchDoc)
+        {
+            if (patchDoc != null)
+            {
+                var user = await _userManager.FindByIdAsync(uid);
+                patchDoc.ApplyTo(user);
+                user.ModifiedOn = DateTime.Now;
+                await _userManager.UpdateAsync(user);
+                return Ok(user);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private User CreateUserFromViewModel(RegisterViewModel model)
+        {
+            return new User()
+            {
+                Email = model.Email,
+                UserName = model.Username,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                NormalizedUserName = model.Username.ToUpper(),
+                NormalizedEmail = model.Email.ToUpper(),
+                ModifiedOn = DateTime.Now,
+                EmailConfirmed = true
+            };
         }
     }
 }
