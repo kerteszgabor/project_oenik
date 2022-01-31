fc3187911770f7d2081080d0b2646cea1b00cd00
kerteszgabor <masodemailcim@gmail.com>
2022-01-31 11:57:05 +0100

    Add missing DTO files

---
+++ project.Domain/DTO/TestResults/AnswerDTO.cs
@@ -0,0 +1,18 @@
+﻿using project.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Domain.DTO.TestResults
+{
+    public class AnswerDTO
+    {
+        public string QuestionID { get; set; }
+        public string TestID { get; set; }
+        public string AnswerText { get; set; }
+        public bool CorrectManually { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ project.Domain/DTO/TestResults/TestStartDTO.cs
@@ -0,0 +1,16 @@
+﻿using project.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Domain.DTO.TestResults
+{
+    public class TestStartDTO
+    {
+        public string QuestionID { get; set; }
+        public string TestID { get; set; }
+        public User User { get; set; }
+    }
+}
