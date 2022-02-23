5fd2228942c66bafba5bd1160b73f421166d6e8c
kerteszgabor <masodemailcim@gmail.com>
2022-02-23 15:06:57 +0100

    Add missing DTO file

---
+++ project.Domain/DTO/TestResults/TestStartStopDTO.cs
@@ -0,0 +1,17 @@
+﻿using project.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Domain.DTO.TestResults
+{
+    public class TestStartStopDTO
+    {
+        public string QuestionID { get; set; }
+        public string TestID { get; set; }
+        public string CourseID { get; set; }
+        public User User { get; set; }
+    }
+}
